da9ac9f0d8211623306afb97afaffde26c6b7b98
willamjonathan <williamjonathan1289@gmail.com>
2023-05-10 21:22:14 +0700

    "LogIn Page (not finished) - only interface"

---
+++ src/components/Login.js
@@ -0,0 +1,56 @@
+import "../styles/Login.css";
+
+function Login() {
+    return(<div>
+        <div class="Login">
+        {/* <span class="MUSIC-MATE">
+        MUSIC MATE
+        </span>
+        <div class="Rectangle-84"></div>
+        <div class="Rectangle-96"></div>
+        <span class="LOG-IN">
+        LOG IN
+        </span>
+        <span class="Create-an-account">
+        Create an account
+        </span> */}
+        <div class="left">
+            <div class="Title">
+                <div class = "MUSIC-MATE">MUSICMATE</div>
+                <div class="Rectangle-84"> . </div>
+                <div class = "Moto">
+                Where Music Comes Alive
+                </div>
+            </div> 
+        </div>
+        <div class = "right">
+            <div class = "user-info">
+                <div class="username">
+                    USERNAME
+                </div>
+                <div class="text-field1">
+                    *****
+                </div>
+                <div class="password">
+                    PASSWORD
+                </div>
+                <div class ="text-field2">
+                    *****
+                </div>
+                <div class = "LogIn">
+                    <div class ='button1'>
+                        LOG IN
+                    </div>
+                    <div class ='create-an-account'>
+                        Create an account
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        </div>
+        
+    </div>)
+}
+
+export default Login;
