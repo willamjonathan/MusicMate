58ed3be220625116fcedce81a439717ed0899e2b
Andrean2305 <andreanhasan@gmail.com>
2023-05-30 16:26:36 +0700

    "added"

--- main.py
+++ main.py
@@ -478,3 +478,25 @@ async def UserNow():
     except Exception as e:
         print("kok error")
         return {"error": str(e)}
+
+#lagu
+class MusicInput(BaseModel):
+    MusicScript: str
+    MusicName: str
+
+simpanLagu = ""
+
+@app.post("/save_music", response_model=dict)
+async def save_music(music: MusicInput):
+    global simpanLagu
+    simpanLagu = music.MusicScript
+
+    print(simpanLagu)
+    # Save the music script to a file or database
+    # You can use music.MusicScript and music.MusicName to access the input values
+    # Implement your logic here to handle the music script storage
+    # For example, you can save the music script to a file
+    with open(f"{music.MusicName}.txt", "w") as f:
+        f.write(music.MusicScript)
+    
+    return {"message": "Music script saved successfully"}
