8543eec2dbb5d2c7df44dc4ec4f46fd9df200efb
willamjonathan <williamjonathan1289@gmail.com>
2023-05-31 16:50:46 +0700

    "profile- edited"

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,8 +2,24 @@ import '../styles/Profile.css'
 import '../styles/Link.css'
 import { Link, useNavigate } from "react-router-dom";
 import LeftSelection from './Leftsection';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import {
+    faUserCircle
+} from '@fortawesome/free-solid-svg-icons'
+import axios from "axios";
+import React, { useState, useEffect} from "react";
 
 function Profile(){
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        const fetchMessage = async () => {
+            const response = await axios.get('http://localhost:8000/');
+            setMessage(response.data.messages);
+        };
+        fetchMessage();
+    }, []);
     return(<div>
         <div class="Profile">
             <LeftSelection/>
@@ -12,17 +28,19 @@ function Profile(){
                     <div class ="profile-profile">Profile</div>
                 </div>
                 <div class="container-profile">
+                    <div class ="box-profile">
+
                     <div class="picture-profile">
-                        
+                    <div class ="profile-logo"><FontAwesomeIcon icon={faUserCircle} /></div>
                     </div>
                     <div class ="details-profile">
                         <div class ="name-profile textprofile">
-                            Name:
+                            Email:
                         </div>
                         <div class="textfield1-profile">
-                            *****
+                            {message}
                         </div>
-                        <div class ="username-profile textprofile">
+                        {/* <div class ="username-profile textprofile">
                             Username:
                         </div>
                         <div class="textfield2-profile">
@@ -33,14 +51,14 @@ function Profile(){
                         </div>
                         <div class="textfield3- profile">
                         *****
-                        </div>
+                        </div> */}
                         
                     </div>
                     <div class="edit-button-profile">
                         EDIT
                     </div>
                 </div>
-
+            </div>
             </div>
         </div>
     </div>)
