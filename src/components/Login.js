124bd45c9d5c89e733b7506baf30d53d7b531c63
Andrean2305 <andreanhasan@gmail.com>
2023-06-01 00:45:29 +0700

    "yes"

--- src/components/Login.js
+++ src/components/Login.js
@@ -61,7 +61,7 @@ function Login() {
       const signupData = { email, password };
 
       try {
-        const response = await fetch("http://localhost:8000/signup", {
+        const response = await fetch("https://music-mates.vercel.app/api/signup", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(signupData),
